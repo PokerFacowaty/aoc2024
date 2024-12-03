08a915ccafcaf8ef1ca0ca459a55ba38112cfcf5
Bartłomiej "PokerFacowaty" Nowak <pokerfacowaty.bn@gmail.com>
2024-12-03 10:34:18 +0100

    Added day 3 part 1

---
+++ 03/01/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+    fileContent, err := os.ReadFile("input")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fileStr := string(fileContent)
+    // NTS: Using backticks to get a raw string
+    r, err := regexp.Compile(`mul\((\d+),(\d+)\)`)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    matches := r.FindAllStringSubmatch(fileStr, -1)
+    total := 0
+    for i := 0; i < len(matches); i++ {
+        intA, err := strconv.Atoi(matches[i][1])
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        intB, err := strconv.Atoi(matches[i][2])
+        if err != nil {
+            log.Fatal(err)
+        }
+        total += intA * intB
+    }
+    fmt.Println(total)
+}
