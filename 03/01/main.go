615d8c7c8859471c0377707d36202d592ea92a24
Bartłomiej "PokerFacowaty" Nowak <pokerfacowaty.bn@gmail.com>
2024-12-03 11:13:45 +0100

    go fmted

--- 03/01/main.go
+++ 03/01/main.go
@@ -9,31 +9,31 @@ import (
 )
 
 func main() {
-    fileContent, err := os.ReadFile("input")
-    if err != nil {
-        log.Fatal(err)
-    }
+	fileContent, err := os.ReadFile("input")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    fileStr := string(fileContent)
-    // NTS: Using backticks to get a raw string
-    r, err := regexp.Compile(`mul\((\d+),(\d+)\)`)
-    if err != nil {
-        log.Fatal(err)
-    }
+	fileStr := string(fileContent)
+	// NTS: Using backticks to get a raw string
+	r, err := regexp.Compile(`mul\((\d+),(\d+)\)`)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    matches := r.FindAllStringSubmatch(fileStr, -1)
-    total := 0
-    for i := 0; i < len(matches); i++ {
-        intA, err := strconv.Atoi(matches[i][1])
-        if err != nil {
-            log.Fatal(err)
-        }
+	matches := r.FindAllStringSubmatch(fileStr, -1)
+	total := 0
+	for i := 0; i < len(matches); i++ {
+		intA, err := strconv.Atoi(matches[i][1])
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        intB, err := strconv.Atoi(matches[i][2])
-        if err != nil {
-            log.Fatal(err)
-        }
-        total += intA * intB
-    }
-    fmt.Println(total)
+		intB, err := strconv.Atoi(matches[i][2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		total += intA * intB
+	}
+	fmt.Println(total)
 }
