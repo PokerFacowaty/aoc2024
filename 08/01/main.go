b5541db0ddc078bb370ceaf82b8fd7a3b6538bcf
Bartłomiej 'PokerFacowaty' Nowak <pokerfacowaty.bn@gmail.com>
2024-12-08 19:05:01 +0100

    Added day 8 part 1

---
+++ 08/01/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type Antenna struct {
+	id   string
+	x, y int
+}
+
+type Antinode struct {
+	x, y int
+}
+
+type Offset struct {
+	x, y int
+}
+
+func findAntinodes(ant1 Antenna, ant2 Antenna, maxX int, maxY int) []Antinode {
+	// 4. 5. 6.
+
+	/* So there's probably a simple way to find a single offset between them
+	   and then usa that to get two "valid" antinodes (in relation to the two
+	   antennas), but calculating two offsets works for now
+	*/
+	var result []Antinode
+	antennasOffset1 := Offset{x: ant2.x - ant1.x, y: ant2.y - ant1.y}
+	antennasOffset2 := Offset{x: ant1.x - ant2.x, y: ant1.y - ant2.y}
+	possibleAntinode1 := Antinode{x: ant1.x + antennasOffset1.x*2,
+		y: ant1.y + antennasOffset1.y*2}
+	possibleAntinode2 := Antinode{x: ant2.x + antennasOffset2.x*2,
+		y: ant2.y + antennasOffset2.y*2}
+
+	if possibleAntinode1.x >= 0 && possibleAntinode1.x <= maxX &&
+		possibleAntinode1.y >= 0 && possibleAntinode1.y <= maxY {
+		result = append(result, possibleAntinode1)
+	}
+	if possibleAntinode2.x >= 0 && possibleAntinode2.x <= maxX &&
+		possibleAntinode2.y >= 0 && possibleAntinode2.y <= maxY {
+		result = append(result, possibleAntinode2)
+	}
+
+	return result
+}
+
+func main() {
+
+	/*
+	   1. Unpack the input into a map[string][]Antenna
+	   2. map[Pos]bool to keep antinodes
+	   3. Loop through antenna letters; loop through its antennas
+	   4. Calculate offset from one to the other
+	   5. Propose antinodes that meets the requirements
+	   6. check if it's within the map
+	   7. If it is, change it to true in the map
+	*/
+
+	// 1.
+	fileContent, err := os.ReadFile("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileStr := strings.Trim(string(fileContent), "\n")
+	fileLines := strings.Split(fileStr, "\n")
+
+	antennas := make(map[string][]Antenna)
+	for y := range fileLines {
+		lineElements := strings.Split(fileLines[y], "")
+		for x := range lineElements {
+			if lineElements[x] == "." {
+				continue
+			}
+			antenna := Antenna{id: lineElements[x], x: x, y: y}
+			antennas[antenna.id] = append(antennas[antenna.id], antenna)
+		}
+	}
+
+	// 2.
+	antinodes := make(map[Antinode]bool)
+	for antennaLetter := range antennas {
+		for i := range antennas[antennaLetter] {
+			currentAntenna := antennas[antennaLetter][i]
+			var tmp []Antenna
+			tmp = append(tmp, antennas[antennaLetter][:i]...)
+			tmp = append(tmp, antennas[antennaLetter][i+1:]...)
+			for j := range tmp {
+				/* Optimization TODO: right now antennas are checked both ways;
+				   once antenna 0 has been checked against all others, antenna
+				   7 doesn't have to check against 0 again */
+				possibleAntinodes := findAntinodes(currentAntenna, tmp[j],
+					len(fileLines[0])-1, len(fileLines)-1)
+				for possibleAntinode := range possibleAntinodes {
+					antinodes[possibleAntinodes[possibleAntinode]] = true
+				}
+			}
+		}
+	}
+	fmt.Println(len(antinodes))
+}
