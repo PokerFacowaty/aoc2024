7c63dcb03fcad6d0d6e26523055da393dc011772
Bartłomiej 'PokerFacowaty' Nowak <pokerfacowaty.bn@gmail.com>
2024-12-08 16:41:01 +0100

    Added day 7 both parts (in Python this time)

---
+++ 07/02/main.py
@@ -0,0 +1,37 @@
+from itertools import product
+with open("input", encoding="utf-8") as f:
+    fileLines = f.readlines()
+
+valid = []
+for line in fileLines:
+    target_number = int(line.split(":")[0])
+    part_numbers = [int(x) for x in line.split(": ")[1].split(" ")]
+    if len(part_numbers) == 2:
+        operator_options = [('*',), ('+',), ('||',)]
+    else:
+        operator_options = list(product(["*", "+", "||"],
+                                        repeat=len(part_numbers) - 1))
+    for options in operator_options:
+        number_so_far = part_numbers[0]
+        i = 0
+        bust = False
+        while i < len(options):
+            if options[i] == "*":
+                number_so_far = number_so_far * part_numbers[i + 1]
+            elif options[i] == "||":
+                number_so_far = int(str(number_so_far)
+                                    + str(part_numbers[i + 1]))
+            else:
+                number_so_far = number_so_far + part_numbers[i + 1]
+
+            i += 1
+            if number_so_far > target_number:
+                bust = True
+                break
+        if bust:
+            continue
+        if number_so_far == target_number:
+            valid.append(number_so_far)
+            break
+
+print(sum(valid))
